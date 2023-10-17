79a9215e9aa585905ddfdce648b849cefed9dbbd
srinandithak <srinandithak21@gmail.com>
2023-10-16 21:09:47 -0500

    detection

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+@Autonomous(name = "Auton", group = "Autonomous")
+public class Auton extends LinearOpMode {
+
+    detectionPipeline pipeline;
+
+
+    public void runOpMode() {
+        //Camera initialization
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        WebcamName OpenCvCamera = hardwareMap.get(WebcamName.class, "frontCamera");
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(OpenCvCamera, cameraMonitorViewId);
+
+        pipeline = new detectionPipeline();
+        camera.setPipeline(pipeline);
+
+        camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+        int position = pipeline.getAnalysis();
+
+        new detectionPipeline();
+
+
+        waitForStart();
+        if (isStopRequested()) {
+            return;
+        }
+
+        telemetry.addData("object detection", position);
+
+        telemetry.update(); 
+
+    }
+}
